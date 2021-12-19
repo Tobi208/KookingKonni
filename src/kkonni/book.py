8cfb3e78e516553b4d441a10cd297c68270c3e27
tobi208 <tobi208@github.com>
2021-12-19 14:11:08 +0100

    request methods

--- src/kkonni/book.py
+++ src/kkonni/book.py
@@ -1,16 +1,11 @@
-from json import loads
+from json import loads, dumps
 
-from flask import Blueprint, render_template, session, redirect, url_for
+from flask import Blueprint, render_template, session, redirect, url_for, request
 
 from kkonni.auth import login_required
 from kkonni.db import get_db
 
 bp = Blueprint("book", __name__)
-
-# https://flask.palletsprojects.com/en/2.0.x/quickstart/
-# https://github.com/pallets/flask/tree/main/examples/tutorial
-# https://flask.palletsprojects.com/en/2.0.x/patterns/appfactories/
-# https://flask-assets.readthedocs.io/en/latest/index.html
 
 
 @bp.route('/')
@@ -33,23 +28,39 @@ def recipe(rid):
     return render_template('recipe/recipe.html', logged_in=session['is_logged_in'], r=r, ings=ingredients)
 
 
-@bp.route('/edit/<int:rid>')
+@bp.route('/edit/<int:rid>', methods=('GET', 'POST'))
 @login_required
 def edit_recipe(rid):
-    cur = get_db().cursor()
-    q = "SELECT * FROM cookbook WHERE rid = ?"
-    cur.execute(q, (rid,))
-    r = cur.fetchone()
-    ingredients = loads(r['ingredients'])
 
-    return render_template('recipe/edit_recipe.html', r=r, ings=ingredients)
+    if request.method == 'GET':
+        cur = get_db().cursor()
+        q = "SELECT * FROM cookbook WHERE rid = ?"
+        cur.execute(q, (rid,))
+        r = cur.fetchone()
+        ingredients = loads(r['ingredients'])
+
+        return render_template('recipe/edit_recipe.html', r=r, ings=ingredients)
+
+    if request.method == 'POST':
+
+        # edit stuff
+
+        return redirect(url_for('book.recipe', rid=1))
 
 
-@bp.route('/new')
+@bp.route('/new', methods=('GET', 'POST'))
 @login_required
 def new_recipe():
 
-    return render_template('recipe/new_recipe.html')
+    if request.method == 'GET':
+
+        return render_template('recipe/new_recipe.html')
+
+    if request.method == 'POST':
+
+        # create stuff
+
+        return redirect(url_for('book.recipe', rid=1))
 
 
 @bp.route('/delete/<int:rid>')
